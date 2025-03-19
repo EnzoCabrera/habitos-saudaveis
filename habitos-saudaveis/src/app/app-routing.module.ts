7574a2868a8c77135433843a695abe28728fdca7
EnzoCabrera <enzo2005cabrera@gmail.com>
2025-03-19 20:52:07 -0300

    initial commit

---
+++ habitos-saudaveis/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { AppLayoutComponent } from "./layout/app.layout.component";
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot([
+            {
+                path: '', component: AppLayoutComponent,
+                children: [
+                    { path: '', loadChildren: () => import('./view/components/dashboard/dashboard.module').then(m => m.DashboardModule) },
+                ]
+            },
+            { path: 'auth', loadChildren: () => import('./view/components/auth/auth.module').then(m => m.AuthModule) },
+        ], { scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled', onSameUrlNavigation: 'reload' })
+    ],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
