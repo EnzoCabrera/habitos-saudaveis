6c8cd8b90e1dbe366b3b13e943cbe4cbe04f7668
Felipe <coutodev@icloud.com>
2025-03-30 14:57:18 -0300

    refactor: adicione HttpClientModule ao AppModule
    Este commit adiciona a importação do HttpClientModule ao módulo principal da aplicação (AppModule). Isso permite que a aplicação utilize o HttpClient do Angular para realizar requisições HTTP.

--- habitos-saudaveis/src/app/app.module.ts
+++ habitos-saudaveis/src/app/app.module.ts
@@ -2,11 +2,12 @@ import { LocationStrategy, PathLocationStrategy } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 import { AppLayoutModule } from './layout/app.layout.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [AppRoutingModule, AppLayoutModule],
+    imports: [AppRoutingModule, AppLayoutModule, HttpClientModule],
     providers: [{ provide: LocationStrategy, useClass: PathLocationStrategy }],
     bootstrap: [AppComponent],
 })
